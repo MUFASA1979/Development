06307b0ebcc3409e0ba896a96cb241beeb1a1ffc
MUFASA1979 <msintriors@gmail.com>
2023-10-02 15:20:07 +0300

    challenge 1 completed

---
+++ react-first-project/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./product";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Cafe</h1>
+      <h3>This is the best Cafe in GurGaon</h3>
+      <img
+        src="https://lh3.googleusercontent.com/r0nEh2deBKYW_XJnFx5rG1_XDcaICQTg-fcbj681GSjdZnCV1Vv3Xc8TM9z26jODvKUYoCtnwwMIuYG-utxbswM08Qo=h450-rw"
+        alt="Cafe Gurgaon"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-first-project/src/product.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "Juicy Lucy Burger",
+    price: "4.000",
+    image: (
+      <img src="https://lh3.googleusercontent.com/tM8c0bgeKuAcld6A_ahTdchJ_Db4i4UIhAvoArrKLX5VLmzbhSE8849rFRdKMv5q6_MsjKwZtJuHBgZerm_itkW31Xgs5T7q_HzY988NAg=w1000-rw"></img>
+    ),
+  },
+  {
+    name: "Oreo Shake",
+    price: "2.000",
+    image: (
+      <img src="https://lh3.googleusercontent.com/DA31TNfiFgTNKIeKjxDahlVyuZWvNv1AJoix83i0bGwI0YVvpxi8Xfb1TReIZaiBKRaT9atAbno3YATIqCM94HE7rRJnN60e--t4YVDz=w512-rw"></img>
+    ),
+  },
+];
